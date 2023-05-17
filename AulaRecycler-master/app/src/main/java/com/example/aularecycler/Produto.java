e6938c37e084bbf861aa055f602c9b9518f95593
ashlyn <102593108+cemeterydriiver@users.noreply.github.com>
2023-05-17 08:03:29 -0300

    first commit

---
+++ AulaRecycler-master/app/src/main/java/com/example/aularecycler/Produto.java
@@ -0,0 +1,46 @@
+package com.example.aularecycler;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Produto {
+    String nome, tipo;
+    float preco;
+
+    public Produto(String nome, String tipo, float preco) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public void salvar(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        reference.child("Produtos").child(nome).setValue(this);
+
+    }
+}
+
